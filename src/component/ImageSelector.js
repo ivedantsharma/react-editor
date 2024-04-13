afee55de6bc14fcb03a896e56f29cbbdb176d54f
ivedantsharma <vedantsharma102004@gmail.com>
2024-04-13 22:54:19 +0530

    changes made

--- src/component/ImageSelector.js
+++ src/component/ImageSelector.js
@@ -1,14 +1,19 @@
-document.querySelector("input").oninput = async (evt) => {
-    try {
-      const file = evt.target.files[0];
-      const bitmap = await createImageBitmap(file);
-      const canvas = document.querySelector("canvas");
-      canvas.width = bitmap.width;
-      canvas.height = bitmap.height;
-      const ctx = canvas.getContext("2d");
-      ctx.drawImage(bitmap, 0, 0);
-    }
-    catch(err) {
-      console.error(err);
-    }
+document.getElementById('inp').onchange = function(e) {
+    var img = new Image();
+    img.onload = draw;
+    img.onerror = failed;
+    img.src = URL.createObjectURL(this.files[0]);
   };
+  function draw() {
+    var canvas = document.getElementById('canvas');
+    canvas.width = this.width;
+    canvas.height = this.height;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(this, 0,0);
+  }
+  function failed() {
+    console.error("The provided file couldn't be loaded as an Image media");
+  }
+
+export default draw;
+
